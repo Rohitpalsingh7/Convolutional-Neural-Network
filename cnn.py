d8863e70cb301968b60319c22c558af837687e9b
Rohitpal Singh <rohitpalsingh91@gmail.com>
2018-04-23 20:37:31 -0400

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -95,7 +95,7 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 
 classifier.fit_generator(training_set,
                         steps_per_epoch=8000,
-                        epochs=5,
+                        epochs=100,
                         validation_data=test_set,
                         validation_steps=2000)
         
